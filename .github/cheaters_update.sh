5a359dd5ac48bbbe1fa9397e20f265408d19e8b3
Doraemon <126883033+ForeverAM@users.noreply.github.com>
2025-11-27 21:08:20 +0800

    Update cheaters_update.sh

--- .github/cheaters_update.sh
+++ .github/cheaters_update.sh
@@ -7,7 +7,7 @@ cheaters_ID=(
 	"76561198379251619｜Vile,&#32;Frequent&#32;name&#32;changes,&#32;using&#32;the&#32;name&#32;of&#32;someone&#32;who&#32;angered&#32;him｜guivalla"
 	"76561199103147131｜Vile,&#32;Frequent&#32;name&#32;changes,&#32;Hid&#32;his&#32;game&#32;library｜Rebely"
 	"76561198787726253｜Vile,&#32;Rebely's&#32;alternate&#32;account,&#32;Frequent&#32;name&#32;changes｜Rebely"
-	"76561199812329207｜Vile,&#32;Pretend&#32;to&#32;be&#32;me,&#32;LMAO,&#32;Frequent&#32;name&#32;changes,&#32;using&#32;the&#32;name&#32;of&#32;someone&#32;who&#32;angered&#32;him｜Johnny"
+	"76561199812329207｜Vile,&#32;Frequent&#32;name&#32;changes,&#32;using&#32;the&#32;name&#32;of&#32;someone&#32;who&#32;angered&#32;him｜Johnny"
 	"76561198861931833｜Vile,&#32;Frequent&#32;name&#32;changes,&#32;using&#32;the&#32;name&#32;of&#32;someone&#32;who&#32;angered&#32;him｜ASJ"
 	"76561199245272853｜Vile｜"
 	76561198862127761
