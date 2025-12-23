cac5f4e2b3645f095e9708f3330fa8848414b728
Doraemon <126883033+ForeverAM@users.noreply.github.com>
2025-12-23 21:02:00 +0800

    Update cheaters_update.sh

--- .github/cheaters_update.sh
+++ .github/cheaters_update.sh
@@ -8,6 +8,7 @@ cheaters_ID=(
 	"76561199103147131｜Vile,&#32;Frequent&#32;name&#32;changes,&#32;Hid&#32;his&#32;game&#32;library｜Rebely"
 	"76561198787726253｜Vile,&#32;Rebely's&#32;alternate&#32;account,&#32;Frequent&#32;name&#32;changes｜Rebely"
 	"76561198861931833｜Vile,&#32;Frequent&#32;name&#32;changes,&#32;using&#32;the&#32;name&#32;of&#32;someone&#32;who&#32;angered&#32;him｜ASJ"
+	76561198832997882
 	76561199077281851
 	76561199796835724
 	76561198367447457
